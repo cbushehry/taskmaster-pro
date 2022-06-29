f653f990f8db4c493929fb86a0c86e3724d308e5
Cameron Bushehry <c.bushehry@gmail.com>
2022-06-29 09:06:17 -0700

    editing process halfway done

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,7 +33,6 @@ var loadTasks = function() {
 
   // loop over object properties
   $.each(tasks, function(list, arr) {
-    console.log(list, arr);
     // then loop over sub-array
     arr.forEach(function(task) {
       createTask(task.text, task.date, list);
@@ -45,8 +44,23 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+$(".list-group").on("click", "p", function() {
+  var text = $(this)
+    .text()
+    .trim();
+});
 
+var textInput = $("<textarea>")
+  .addClass("form-control")
+  .val(text);
 
+  $(this).replaceWith(textInput);
+
+  textInput.trigger("focus");
+
+  $(".list-group").on("blur", "textarea", function() {
+
+  });
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
